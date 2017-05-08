612b018108bfce69eac3942a1916933675e65b71
Pavlo-laptop <kasianchuk.pavlo@gmail.com>
2017-05-08 23:22:18 +0300

    Add user controller && view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to root_path, :alert => "Access denied."
+    end
+  end
+
+end
